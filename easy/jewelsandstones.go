ac1540f0daa9f225a620850d1688008075234afb
smallpigex <smallpigex@gmail.com>
2018-08-22 22:51:17 +0800

    rename

--- easy/jewelsandstones.go
+++ easy/jewelsandstones.go
@@ -1,15 +1,15 @@
 package main
 
 func numJewelsInStones(J string, S string) int {
-	numOfjewel := 0
+	numOfJewels := 0
 
 	for _, jewel := range J {
 		for _, stone := range S {
 			if stone == jewel {
-				numOfjewel++
+				numOfJewels++
 			}
 		}
 	}
 
-	return numOfjewel
+	return numOfJewels
 }
